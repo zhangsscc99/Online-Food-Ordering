a9a4fbfaca5721c8242b398b66b1c384aa2f7816
Yuting Wang <yw4554@nyu.edu>
2024-03-23 12:07:10 -0500

    cache evict

--- src/main/java/com/laioffer/onlineorder/service/CartService.java
+++ src/main/java/com/laioffer/onlineorder/service/CartService.java
@@ -9,6 +9,8 @@ import com.laioffer.onlineorder.model.OrderItemDto;
 import com.laioffer.onlineorder.repository.CartRepository;
 import com.laioffer.onlineorder.repository.MenuItemRepository;
 import com.laioffer.onlineorder.repository.OrderItemRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,7 +34,7 @@ public class CartService {
         this.orderItemRepository = orderItemRepository;
     }
 
-
+    @CacheEvict(cacheNames = "cart", key = "#customerId")
     @Transactional
     public void addMenuItemToCart(long customerId, long menuItemId) {
         CartEntity cart = cartRepository.getByCustomerId(customerId);
@@ -54,15 +56,14 @@ public class CartService {
         cartRepository.updateTotalPrice(cart.id(), cart.totalPrice() + menuItem.price());
     }
 
-
+    @Cacheable("cart")
     public CartDto getCart(Long customerId) {
         CartEntity cart = cartRepository.getByCustomerId(customerId);
         List<OrderItemEntity> orderItems = orderItemRepository.getAllByCartId(cart.id());
         List<OrderItemDto> orderItemDtos = getOrderItemDtos(orderItems);
         return new CartDto(cart, orderItemDtos);
     }
-
-
+    @CacheEvict(cacheNames = "cart")
     @Transactional
     public void clearCart(Long customerId) {
         CartEntity cartEntity = cartRepository.getByCustomerId(customerId);
