6e78646fb982672b12cca57bd0602551d8b59f2e
Yuting Wang <yw4554@nyu.edu>
2024-03-23 20:12:18 -0500

    again

--- src/main/java/com/laioffer/onlineorder/repository/CustomerRepository.java
+++ src/main/java/com/laioffer/onlineorder/repository/CustomerRepository.java
@@ -25,4 +25,9 @@ public interface CustomerRepository extends ListCrudRepository<CustomerEntity, L
     @Modifying
     @Query("UPDATE customers SET first_name = :firstName, last_name = :lastName WHERE id = :id")
     void updateNameById(long id, String firstName, String lastName);
+
+
+    @Modifying
+    @Query("UPDATE customers SET first_name = :firstName, last_name = :lastName WHERE email = :email")
+    void updateNameByEmail(String email, String firstName, String lastName);
 }
